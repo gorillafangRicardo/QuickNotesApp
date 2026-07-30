032de23b4c425488b149f640c1b2eaa4a314f84c
gorillafangRicardo <ricardocampos@gorillafang.studio>
2026-07-29 21:52:02 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+let notes = []
+
+function saveNote(event){
+    event.preventDefault()
+
+    const title = document.getElementById('noteTitle').value.trim();
+    const content = document.getElementById('noteContent').value.trim();
+
+    notes.unshift({
+        id: generateId(),
+        title: title,
+        content: content
+    })
+
+    saveNotes()
+}
+
+function generateId(){
+    return Date.now().toString()
+}
+
+function saveNotes(){
+    localStorage.setItem('quickNotes', JSON.stringify(notes))
+}
+
+function renderNotes(){
+    const notesContainer = document.getElementById('notesContainer');
+
+    if(notes.length === 0){
+        notesContainer.innerHTML = `
+        <div class="empty/state">
+    <h2>No Notes Yet</h2>
+    <p>Create your first note to get started!</p>
+    <button class="add-note-btn" onclick="openNoteDialog()">Add Your First Note</button>
+</div>
+        `
+
+        return
+    }
+
+    notesContainer.innerHTML = notes.map (note => `
+        <div class="note-card">
+        <h3 class="note-title">${note.title}</h3>
+        <p class="note-content">${note.content}</p>
+        </div>
+        `).join('')
+}
+
+function openNoteDialog(){
+    const dialog = document.getElementById('noteDialog');
+    const titleInput = document.getElementById('noteTitle');
+    const contentInput = document.getElementById('noteContent');
+
+    dialog.showModal()
+    titleInput.focus()
+}
+
+function closeNoteDialog(){
+    document.getElementById('noteDialog').close()
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    document.getElementById('noteForm').addEventListener('submit', saveNote)
+
+    document.getElementById('noteDialog').addEventListener('click', function(event){
+        if(event.target === this){
+            closeNoteDialog()
+        }
+    })
+})
